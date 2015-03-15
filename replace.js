2e883fd807a32373afe61385f4b0e49496a35beb
emile21 <MagicHack@iMac.local>
2015-03-15 19:34:42 -0400

    a

--- replace.js
+++ replace.js
@@ -6,7 +6,7 @@ function rep(){
     alert("test")
     var head = '<meta charset="utf-8"> <title>Bienvenue, Émile Larose-Levac</title> <link rel="stylesheet" href="https://rawgit.com/emile21/test/master/style.css" type="text/css">';
     
-    var body = '<div id="container"> <div id="auto"> <h1>&lt; Bienvenue sur le pluriportail d\'Emile Larose-Levac &gt;</h1> <span class="red">Ne pas entrer sans permission</span> </div> <form name="FormLogin" method="post" action="Login.srf?Etape=2&Login=1" onsubmit="return valider();"> <input type="hidden" name="NomLogin" value="3207578" /> <label for="pass">Mot de passe: </label> <input type="password" name="MotPasse" placeholder="12345678 lol" id="pass" autofocus> <br> <br> <br> <input type="submit" id="login" value=""> </form> <div class="blink_me" id="error"></div> </div> <script src="https://rawgit.com/emile21/test/master/script.js"></script> <script src="https://rawgit.com/emile21/test/master/write.js"></script>';
+    var body = '<div id="container"> <div id="auto"> <h1>Bienvenue sur le pluriportail d\'Emile Larose-Levac </h1> <span class="red">Ne pas entrer sans permission</span> </div> <form name="FormLogin" method="post" action="Login.srf?Etape=2&Login=1" onsubmit="return valider();"> <input type="hidden" name="NomLogin" value="3207578" /> <label for="pass">Mot de passe: </label> <input type="password" name="MotPasse" placeholder="12345678 lol" id="pass" autofocus> <br> <br> <br> <input type="submit" id="login" value=""> </form> <div class="blink_me" id="error"></div> </div> <script src="https://rawgit.com/emile21/test/master/script.js"></script> <script src="https://rawgit.com/emile21/test/master/write.js"></script>';
     
     document.head.innerHTML = head;
     document.body.innerHTML = body;
@@ -289,7 +289,7 @@ var nb = 0;
 function valider() {
     var erreur = document.getElementById("error");
     if (3 - nb <= 0) {
-        var text = "Nombre d'essais dépassés";
+        var text = "Nombre d'essais depasses";
         erreur.innerHTML = text;
         return false;
     }
@@ -303,7 +303,7 @@ function valider() {
         erreur.innerHTML = text;
         erreur.style.visibility = "visible";
         if (3 - nb <= 0) {
-            var text = "Nombre d'essais dépassés";
+            var text = "Nombre d'essais depasses";
             erreur.innerHTML = text;
             return false;
         }
