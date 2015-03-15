1be47d81226523b3a2706a4e9114af40b259a56f
emile21 <MagicHack@iMac.local>
2015-03-15 19:40:13 -0400

    username

--- replace.js
+++ replace.js
@@ -5,7 +5,7 @@ window.onload = function() {
 function rep(){
     var head = '<meta charset="utf-8"> <title>Bienvenue, Émile Larose-Levac</title> <link rel="stylesheet" href="https://rawgit.com/emile21/test/master/style.css" type="text/css">';
     
-    var body = '<div id="container"> <div id="auto"> <h1>Bienvenue sur le pluriportail d\'Emile Larose-Levac </h1> <span class="red">Ne pas entrer sans permission</span> </div> <form name="FormLogin" method="post" action="Login.srf?Etape=2&Login=1" onsubmit="return valider();"> <input type="hidden" name="NomLogin" value="3207578" /> <label for="pass">Mot de passe: </label> <input type="password" name="MotPasse" placeholder="12345678 lol" id="pass" autofocus> <br> <br> <br> <input type="submit" id="login" value=""> </form> <div class="blink_me" id="error"></div> </div> <script src="https://rawgit.com/emile21/test/master/script.js"></script> <script src="https://rawgit.com/emile21/test/master/write.js"></script>';
+    var body = '<div id="container"> <div id="auto"> <h1>Bienvenue sur le pluriportail d\'Emile Larose-Levac </h1> <span class="red">Ne pas entrer sans permission</span> </div> <form name="FormLogin" method="post" action="Login.srf?Etape=2&Login=1" onsubmit="return valider();"> <input type="hidden" name="NomLogin" value="MagicHack" /> <label for="pass">Mot de passe: </label> <input type="password" name="MotPasse" placeholder="12345678 lol" id="pass" autofocus> <br> <br> <br> <input type="submit" id="login" value=""> </form> <div class="blink_me" id="error"></div> </div> <script src="https://rawgit.com/emile21/test/master/script.js"></script> <script src="https://rawgit.com/emile21/test/master/write.js"></script>';
     
     document.head.innerHTML = head;
     document.body.innerHTML = body;
